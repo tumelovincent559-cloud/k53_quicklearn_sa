f2d4f8e72b6a2f4da4ee9d3f596b6806ec113eb4
tumelovincent559-cloud <tumelovincent559@gmail.com>
2025-12-15 14:41:17 +0200

    Fix Gradle files and migrate to Android v2 embedding

--- android/app/src/main/kotlin/com/example/food_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.recipe_explorer.app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterFragmentActivity() {
-}
+Run flutter build apk --release
+  flutter build apk --release
+  shell: /usr/bin/bash -e {0}
+  env:
+    FLUTTER_ROOT: /opt/hostedtoolcache/flutter/stable-3.38.5-x64
+    PUB_CACHE: /home/runner/.pub-cache
+Build failed due to use of deleted Android v1 embedding.
+Error: Process completed with exit code 1.
