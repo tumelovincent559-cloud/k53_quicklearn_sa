d01b271929802f0ffd19e8cc07f21b96225ec183
tumelovincent559-cloud <tumelovincent559@gmail.com>
2025-12-15 14:49:24 +0200

    Migrate MainActivity to Android v2 embedding

--- android/app/src/main/kotlin/com/example/food_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_app/MainActivity.kt
@@ -1,8 +1,6 @@
-Run flutter build apk --release
-  flutter build apk --release
-  shell: /usr/bin/bash -e {0}
-  env:
-    FLUTTER_ROOT: /opt/hostedtoolcache/flutter/stable-3.38.5-x64
-    PUB_CACHE: /home/runner/.pub-cache
-Build failed due to use of deleted Android v1 embedding.
-Error: Process completed with exit code 1.
+package com.example.food_app   // <-- match your actual package name
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
